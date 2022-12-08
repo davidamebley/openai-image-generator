e08ce8b9759e01f5e3889fe9118e80930288ad86
David Amebley <davameb@gmail.com>
2022-12-08 17:24:03 +0200

    Create Static css and js files for frontend

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-//Enable body parser to allow body data from frontend /form
+//Enable body parser to allow body data from client /form
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add route
 app.use('/openai', require('./routes/openaiRoutes'));
 
+// Start Server
 app.listen(port, () => console.log(`Server started on port ${port}`));
